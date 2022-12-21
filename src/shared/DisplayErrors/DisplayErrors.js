d8a006d233cd7dac0bee3d195667590054f7b534
BadgerSIO <saadibneomar@gmail.com>
2022-12-21 10:29:53 +0600

    loader design updated

--- src/shared/DisplayErrors/DisplayErrors.js
+++ src/shared/DisplayErrors/DisplayErrors.js
@@ -1,23 +1,33 @@
 import React, { useContext } from "react";
-import { useRouteError } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 import { AuthContext } from "../../context/AuthProvider";
 
 const DisplayErrors = () => {
   const { logout } = useContext(AuthContext);
   const error = useRouteError();
+  const navigate = useNavigate();
+  const logoutt = () => {
+    logout();
+    navigate("/login");
+  };
   return (
-    <div>
-      <p className="text-2xl md:text-3xl text-center">Something went wrong</p>
-      <p className="text-lg md:text-xl text-center">
-        {error.statusText || error.message}
-      </p>
-      <p>
-        Please Try{" "}
-        <span onClick={logout} className="text-red-500 font-bold">
-          logout
-        </span>{" "}
-        and Login
-      </p>
+    <div className="h-screen flex justify-center items-center">
+      <div>
+        <p className="text-2xl md:text-3xl text-center">Something went wrong</p>
+        <p className="text-lg md:text-xl text-center">
+          {error.statusText || error.message}
+        </p>
+        <p>
+          Please Try{" "}
+          <span
+            onClick={logoutt}
+            className="text-red-500 font-bold cursor-pointer"
+          >
+            logout
+          </span>{" "}
+          and Login
+        </p>
+      </div>
     </div>
   );
 };
--- src/shared/Loader/Loader.js
+++ src/shared/Loader/Loader.js
@@ -3,7 +3,7 @@ import { FaCog } from "react-icons/fa";
 
 const Loader = () => {
   return (
-    <div className="h-full flex justify-center items-center">
+    <div className="h-screen w-screen fixed bg-white z-40 left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 flex justify-center items-center">
       <FaCog className="text-5xl animate-spin"></FaCog>
       <p className="ml-4">Loading</p>
     </div>
